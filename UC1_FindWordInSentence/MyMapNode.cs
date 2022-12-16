1c571d59eab50162a4189e7a7acc222ade01d4d6
SoumyaD27 <soumyad233@gmail.com>
2022-12-16 16:28:44 +0530

    [Find word in sentence]

---
+++ UC1_FindWordInSentence/MyMapNode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1_FindWordInSentence
+{
+    public class MyMapNode<K, V>
+    {
+        LinkedList<KeyValue<K, V>> list = new LinkedList<KeyValue<K, V>>();
+        public void Add(K key, V value)
+        {
+            KeyValue<K, V> keyvalue = new KeyValue<K, V>() { key = key, value = value };
+            list.AddLast(keyvalue);
+
+        }
+        public void GetFrequency()
+        {
+            int count = 0;
+            if (list == null)
+            {
+                Console.WriteLine("Hash Table is empty");
+                return;
+            }
+            foreach (KeyValue<K, V> item in list)
+            {
+                foreach (KeyValue<K, V> value in list)
+                {
+                    if (item.value.Equals(value.value))
+
+                        count++;
+            }
+            Console.WriteLine("Count of " + item.value + "is " + count);
+            count = 0;
+        }
+    }
+}
+    public struct KeyValue<K, V>
+        {
+        public K key;
+        public V value;
+        }
+}
---
+++ UC1_FindWordInSentence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace UC1_FindWordInSentence
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<int, string> hash = new MyMapNode<int, string>();
+            hash.Add(0, "To");
+            hash.Add(1, "be");
+            hash.Add(2, "or");
+            hash.Add(3, "not");
+            hash.Add(4, "to");
+            hash.Add(5, "be");
+            hash.GetFrequency();
+        }
+    }
+}
