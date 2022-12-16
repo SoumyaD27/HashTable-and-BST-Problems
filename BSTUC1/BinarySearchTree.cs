2b44e1a133c14222bff95c414a03342a98f8e0c5
SoumyaD27 <soumyad233@gmail.com>
2022-12-16 17:08:20 +0530

    [Create a BST]

---
+++ BSTUC1/BinarySearchTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTUC1
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+
+    {
+        public T data;
+        public BinarySearchTree<T> left;
+        public BinarySearchTree<T> right;
+        public BinarySearchTree(T data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+        int leftIndex = 0, rightIndex = 0;
+        public void AddNode(T Value)
+        {
+            T NodeValue = this.data;
+            if(NodeValue.CompareTo(Value) >  0 )
+            {
+                if (this.left != null)
+                    this.left = new BinarySearchTree<T>(Value);
+                else
+                    this.left.AddNode(Value);
+            }
+            else
+            {
+                if (this.right == null)
+                    this.right = new BinarySearchTree<T>(Value);
+                else
+                    this.right.AddNode(Value);
+            }
+        }
+        public void Display()
+        {
+            if (this.left != null)
+            {
+                this.leftIndex++;
+                this.left.Display();
+            }
+            Console.WriteLine(this.data);
+            if(this.right != null)
+            {
+                this.rightIndex++;
+                this.right.Display();
+            }
+        }
+    }
+}
---
+++ BSTUC1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BSTUC1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>(56);
+            tree.AddNode(30);
+            tree.AddNode(70);
+            tree.Display();
+        }
+    }
+}
